343979573dd75c98d3c5244f7d38a9082f30b33d
widiapuspitasar <puspitasariwidia@gmail.com>
2024-01-19 14:38:53 +0700

    testing1

---
+++ module-5/src/component/StatusPokemon/index.tsx
@@ -0,0 +1,43 @@
+import { Card, CardContent, Grid, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material"
+import { PokemonDetail } from "../../interface"
+
+interface StatusPokemonProps {
+    pokemon: PokemonDetail
+}
+
+const StatusPokemon = ({pokemon}: StatusPokemonProps) => {
+    return(
+        <>
+        <Card>
+      <CardContent>
+        <Grid
+          container
+          justifyContent="center"
+          spacing={2}
+        >
+          <Grid item>
+          {pokemon ? (
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                {pokemon.stats.map((stat) => {
+                  return <TableCell sx={{textTransform: "capitalize"}}>{stat.stat.name}</TableCell>
+                })}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {pokemon.stats.map((stat) => {
+                return <TableCell>{stat.base_stat}</TableCell>
+              })}
+            </TableBody>
+          </Table>
+        ) : null}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+        </>
+    )
+}
+
+export default StatusPokemon
---
+++ module-5/src/component/index.ts
@@ -0,0 +1,7 @@
+export { default as Navbar } from './Navbar'
+export { default as PokemonList } from './PokemonList'
+export { default as CardPokemon } from './CardPokemon'
+// export { default as DetailPokemon } from './DetailPokemon'
+export { default as AvatarPokemon } from './AvatarPokemon'
+export { default as BasicInfoPokemon } from './BasicInfoPokemon'
+export { default as StatusPokemon } from './StatusPokemon'

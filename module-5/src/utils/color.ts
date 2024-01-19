f96e195a56e7ab6a4a6cd78eddba6f0228874791
Widia Puspitasari <136142947+widiapuspitasar@users.noreply.github.com>
2024-01-19 17:59:09 +0700

    Delete module-5 directory
    error

--- module-5/src/utils/color.ts
+++
@@ -1,9 +0,0 @@
-import { FastAverageColor  } from "fast-average-color";
-
-export const getColorFromUrl = async (url: string) => {
-    const fastColor = new FastAverageColor();
-    const color = await fastColor.getColorAsync(url);
-    if (color.error) return null;
-    return color.hex || null;
-
-}
